0b4b9f95970403c896a6e8faa0d2106fc6eb1ee3
BreakfastAtKyrils <kyrildr@gmail.com>
2022-04-06 19:40:09 +0000

    assertObjectsEqual

---
+++ assertObjectsEqual.js
@@ -0,0 +1,27 @@
+const eqObjects = function(object1, object2) {
+  //first we check if they have the same amount of keys
+  if (Object.keys(object1).length !== Object.keys(object2).length) {
+    return false
+  }
+
+  for (let key in object1) {
+    if (object1[key] !== object2[key]) {
+      return false;
+    }
+  }
+  return true
+};
+
+const assertObjectsEqual = function(actual, expected) {
+  const inspect = require('util').inspect;
+  if (!eqObjects(actual, expected)) {
+    return (console.log(`❌❌❌ Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`));
+  } 
+  return (console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`));
+};
+
+assertObjectsEqual({ a: '1', b: 2 }, { b: 2, a: '1' })
+assertObjectsEqual({ a: '1', b: 2 }, { b: 2, a: '2' })
+
+
+
