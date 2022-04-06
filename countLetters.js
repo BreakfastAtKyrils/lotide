87c784282f3ee3b73cd306749c1c1370aa75e4c2
BreakfastAtKyrils <kyrildr@gmail.com>
2022-04-06 17:51:21 +0000

    countLetters

---
+++ countLetters.js
@@ -0,0 +1,22 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(string) {
+  let returnObject = {}
+  for (let char of string) {
+        if (char in returnObject) {
+          returnObject[char] ++;
+        }
+        else {
+          returnObject[char] = 1
+        }
+      }
+  return returnObject;
+}
+
+console.log(countLetters('ABBCCCDDDD'))
